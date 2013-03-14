5a40c792c07e5933364ac89ed2f8e2d9c6e7346b
Muhammad Khan <mhk98@cornell.edu>
2013-03-14 03:35:33 -0400

    meanfilter and stuff

---
+++ mean_filter.ml
@@ -0,0 +1,34 @@
+module P = Data_parser.DataParser
+
+let to_stencil k =
+  let rec helper k' =
+    if k' = 0 then []
+    else (1. /. (float_of_int k))::helper(k' - 1)
+  in
+  helper k
+
+let all_data = P.parse "test.txt"
+
+(*Part 1*)
+
+let c1 = Analysis.choose_rand all_data
+let c2 = Analysis.choose_rand all_data
+let c3 = Analysis.choose_rand all_data
+
+let s = to_stencil 3
+
+let c1' = Analysis.convolution s c1
+let c2' = Analysis.convolution s c2
+let c3' = Analysis.convolution s c3
+
+let intify (lst : float list) : (int*int) array =
+  let i = ref (-1) in
+  let f (fl : float) =
+    incr i;
+    (10*(!i), 150*(int_of_float fl))
+  in
+  Array.of_list (List.map f lst)
+
+let run () = 
+  Graphics.open_graph "";
+  Graphics.plots (intify (Array.get all_data 0));
