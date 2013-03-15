3ed80b2b26390eeaf16bbc3e2020d2599f94fea3
aab227 <arjunbiddanda@gmail.com>
2013-03-14 20:21:27 -0400

    remade build script

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 #Builds CS 5540 Project 2
 
 #I still need to see if this works and creates an appropriate toplevel. 
-ocamlc -c io.ml data_parser.ml analysis.ml mean_filter.ml
+ocamlc -c io.ml data_parser.ml analysis.ml mean_filter.ml autocorrelation.ml harr_transform.ml
 if [$? -ne 0]; then
   echo 'Something here does not compile'
 else 
-  ocamlmktop -o toplevel.exe io.cmo data_parser.cmo analysis.cmo mean_filter.cmo
+  ocamlmktop -o toplevel.exe io.cmo data_parser.cmo analysis.cmo mean_filter.cmo autocorrelation.cmo harr_transform.cmo
   if [$? -ne 0]; then
     echo 'Something here does not compile'
   else  
